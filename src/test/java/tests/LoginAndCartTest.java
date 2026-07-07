fe2e9242222ccfe155ee82e48b1281c33a126f2f
athulya-shahul <athulyashahul123@gmail.com>
2026-07-07 14:44:11 +0530

    Update LoginAndCartTest.java

--- src/test/java/tests/LoginAndCartTest.java
+++ src/test/java/tests/LoginAndCartTest.java
@@ -8,8 +8,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import java.time.Duration;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.chrome.ChromeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
